05aa9089190fd72334bac71bbeecaa8941760cd5
Sofyan Thayf <sofyanthayf@users.noreply.github.com>
2018-01-18 16:58:50 +0800

    update

---
+++ application/views/layout/header.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>PWEB171 Blogs</title>
+	</head>
+	<body>
